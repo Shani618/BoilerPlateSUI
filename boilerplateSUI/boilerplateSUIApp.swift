4ac1e6cc42202b7afad91a55577f3201cfd387ae
Ehtisham <ahteshamshani618@gmail.com>
2024-12-12 21:54:59 +0500

    Initial Commit

---
+++ boilerplateSUI/boilerplateSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  boilerplateSUIApp.swift
+//  boilerplateSUI
+//
+//  Created by Shani on 12/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct boilerplateSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ boilerplateSUITests/boilerplateSUITests.swift
@@ -0,0 +1,17 @@
+//
+//  boilerplateSUITests.swift
+//  boilerplateSUITests
+//
+//  Created by Shani on 12/12/2024.
+//
+
+import Testing
+@testable import boilerplateSUI
+
+struct boilerplateSUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
